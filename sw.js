335398a8217c57ba89de8357693cb161155bfe60
tacticalterrordivision <TTDHQ@proton.me>
2026-08-16 10:55:10 +0930

    Implement service worker caching and fetch logic
    Add service worker installation and fetch event handling.

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('ttd-hq-v1').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './styles.css',
+      './app.js',
+      './manifest.json'
+    ]))
+  );
+});
 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
